5973d50a2e19a4e746ddcb408ad7a1456817bcfe
Monzoor Morshed Mahmud <mmtamal@gmail.com>
2018-12-25 02:59:47 +0600

    Twisker new build

---
+++ twisker/precache-manifest.d9520d8b5624d21308e9696ee58d4391.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87cb5368ff2529ca7fec",
+    "url": "/twisker/static/js/runtime~main.87cb5368.js"
+  },
+  {
+    "revision": "06d0ec2c4d7a6b0edbf2",
+    "url": "/twisker/static/js/main.06d0ec2c.chunk.js"
+  },
+  {
+    "revision": "f0a698be9a2933e4d656",
+    "url": "/twisker/static/js/1.f0a698be.chunk.js"
+  },
+  {
+    "revision": "06d0ec2c4d7a6b0edbf2",
+    "url": "/twisker/static/css/main.da78aa43.chunk.css"
+  },
+  {
+    "revision": "ade82399f456455cdfdae9da0cc29f7c",
+    "url": "/twisker/index.html"
+  }
+];
--- twisker/service-worker.js
+++ twisker/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4792abf11c829cddbc9be9ed72018876.js"
+  "/twisker/precache-manifest.d9520d8b5624d21308e9696ee58d4391.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/twisker/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
